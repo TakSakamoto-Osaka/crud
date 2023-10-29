688ed93eaaa44e515e0857e7eb613437f81b6d3f
Tak.Sakamoto <sakamototak.job@gmail.com>
2023-10-29 14:16:29 +0900

    8章

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
 from .models import Product
 
 class TopView(TemplateView):
@@ -9,4 +9,7 @@ class ProductListView(ListView):
     model = Product
     # template_name = "list.html"
     paginate_by = 3
-    
+   
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
