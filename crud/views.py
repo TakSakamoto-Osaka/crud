90f486662fe9dbcc9bd33de16857f910ebd12653
Tak.Sakamoto <sakamototak.job@gmail.com>
2023-12-29 08:14:00 +0900

    13章

--- crud/views.py
+++ crud/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy
 from .models import Product
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
 
 class TopView(TemplateView):
     template_name = "top.html"
@@ -15,28 +18,35 @@ class TopView(TemplateView):
     '''
 
 
-class ProductListView(ListView):
+class ProductListView(LoginRequiredMixin, ListView):
     model = Product
     # template_name = "list.html"
     paginate_by = 3
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     fields = '__all__'
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     fields = '__all__'
     template_name_suffix = '_update_form'
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('list')
-
-
-class ProductDetailView(DetailView):
+    
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
+
+class LoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+    
+class LogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'top.html'
+
     
