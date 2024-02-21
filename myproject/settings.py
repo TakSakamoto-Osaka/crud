a4e9b95df33ea8e16c48a98d36e155f33b25d4b2
Tak.Sakamoto <sakamototak.job@gmail.com>
2024-02-21 17:46:02 +0900

    許可ホスト設定変更

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z$uls(9(_td9(e#fz@33gg&bc3an7#a_-mk&sh-kzaz08@!xm0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
